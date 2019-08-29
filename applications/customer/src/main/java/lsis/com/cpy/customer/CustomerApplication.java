3806d976ce1f064e0b587d30a33c66b0726cc4a9
skshon <skshon@lsis.com>
2019-08-29 16:15:24 +0900

    Add Circuit Breaker

--- applications/customer/src/main/java/lsis/com/cpy/customer/CustomerApplication.java
+++ applications/customer/src/main/java/lsis/com/cpy/customer/CustomerApplication.java
@@ -3,11 +3,13 @@ package lsis.com.cpy.customer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class CustomerApplication {
--- applications/customer/src/main/java/lsis/com/cpy/customer/OrdersClient.java
+++ applications/customer/src/main/java/lsis/com/cpy/customer/OrdersClient.java
@@ -1,9 +1,12 @@
 package lsis.com.cpy.customer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestOperations;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -11,6 +14,7 @@ public class OrdersClient {
 
     private String orderUrl;
     private RestOperations restOperations;
+    private Map<Long, List<OrderInfo>> ordersCache = new ConcurrentHashMap<>();
 
     public OrdersClient(String orderUrl, RestOperations restOperations) {
         this.orderUrl = orderUrl;
@@ -32,10 +36,18 @@ public class OrdersClient {
         return restOperations.exchange(orderUrl, GET, null, albumListType).getBody();
     }
 
+    @HystrixCommand(fallbackMethod = "getOrdersFromCache")
     public List<OrderInfo> getOrdersByCustomerId(long customerId) {
         ParameterizedTypeReference<List<OrderInfo>> albumListType = new ParameterizedTypeReference<List<OrderInfo>>() {
         };
 
-        return restOperations.exchange(orderUrl + "/customer/" + customerId, GET, null, albumListType).getBody();
+        List<OrderInfo> orders = restOperations.exchange(orderUrl + "/customer/" + customerId, GET, null, albumListType).getBody();
+        ordersCache.put(customerId, orders);
+
+        return orders;
+    }
+
+    public List<OrderInfo> getOrdersFromCache(long customerId) {
+        return ordersCache.get(customerId);
     }
 }
